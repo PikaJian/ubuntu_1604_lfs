0d4f66c45892c41af9bdfcccf12fe8953b818051
pikajian <pikajian@actions-micro.com>
2023-03-16 15:44:38 +0800

    Update code-server install script

--- install_packages.sh
+++ install_packages.sh
@@ -2,3 +2,6 @@
 python3 get-pip.py
 #python 3.5 can not used
 pip3 install compiledb
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
+curl -fsSL https://code-server.dev/install.sh | sh
+dpkg -x code-server_4.10.1_amd64.deb
